076de774f96628b6977d81fccdae3445964a492b
546702083 <32324857+546702083@users.noreply.github.com>
2017-10-19 18:51:09 +0800

    fgbf
    xdl

---
+++ www/views/shop/shop-edit.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+  'use strict';
+  angular.module('starter.controllers').controller('ShopEditCtrl',['$scope','$stateParams','localStorageService',function ($scope,$stateParams,localStorageService) {
+    $scope.title = $stateParams.title;
+    $scope.property = $stateParams.property;
+    $scope.shop = localStorageService.get('Shop',{
+      name:'谢栋林',
+      ab:'',
+      name1:'谢栋林',
+      phone:'13003800758',
+      phone1:'13003800758',
+      createTime:'2017-9-28 15:30:00',
+      email:'546702083@qq.com',
+      hylx:'111'
+
+    });
+    $scope.save = function () {
+      localStorageService.update('Shop',$scope.shop);
+    };
+  }]);
+})();
